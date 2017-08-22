0987fe05157f2b501cd8d41f5344e938098a95e1
Xeonice <ad546971975@gmail.com>
2017-08-23 00:47:35 +0800

    Google 街景API添加，任务1／3完成

--- js/script.js
+++ js/script.js
@@ -15,6 +15,17 @@ function loadData() {
 
     // YOUR CODE GOES HERE!
 
+    var streetStr = $('#street').val(); //获取street栏输入信息
+    var cityStr = $('#city').val(); //获取city栏输入信息
+    var address = streetStr + "," + cityStr; //组合street信息，city信息，合成position信息
+
+    var API_KEY = '&key=AIzaSyBDBBteQ2_faToWAPcPLZyWG_zJBwFSwyw'; //API密钥
+
+    $greeting.text("So, you want to live at " + address + '?'); //gretting信息
+
+    var streetUrl = "'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=" + address + API_KEY; //街景照片Url = 初始信息 + address + API密钥
+    $body.append('<img class="bgimg" src=' + streetUrl + "'>"); //在body最后加入图片html代码
+
     return false;
 };
 
